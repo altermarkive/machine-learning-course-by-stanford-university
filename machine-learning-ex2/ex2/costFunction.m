a9f18b0a3ced320300587fd043c64925d0722e29
altermarkive <altermarkive@gmail.com>
2016-10-19 12:07:54 +0200

    Exercise 2 - solved

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -20,6 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X * theta);
+J = sum(-y' * log(h) - (1 - y') * log(1 - h)) / m;
+grad = (X' * (h - y)) / m;
 
 
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,6 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X * theta);
+cost = sum(-y .* log(h) - (1 - y) .* log(1 - h));
+J = (cost / m) + (lambda / (2 * m)) * sum(theta(2:size(theta)) .^ 2);
+extra = lambda * theta;
+extra(1) = 0;
+grad = ((X' * (h - y)) + extra) / m;
 
 
 
