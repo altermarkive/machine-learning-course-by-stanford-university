8c3fa4a5b50ca05b01967bae9d5bfba4c93996f1
altermarkive <altermarkive@gmail.com>
2016-10-27 14:52:23 +0000

    Exercise 3 - solved

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -9,8 +9,8 @@ function [J, grad] = lrCostFunction(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+%J = 0;
+%grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -36,6 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
+cost = sum(-y .* log(h) - (1 - y) .* log(1 - h));
+J = (cost / m) + (lambda / (2 * m)) * sum(theta(2:size(theta)) .^ 2);
+extra = lambda * theta;
+extra(1) = 0;
+grad = ((X' * (h - y)) + extra) / m;
 
 
 
--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -12,7 +12,7 @@ m = size(X, 1);
 num_labels = size(all_theta, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+%p = zeros(size(X, 1), 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
@@ -30,6 +30,8 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
+h = X * all_theta';
+[~, p] = max(h, [], 2);
 
 
 
