b62d2d06b08cc4be759d4f58645f1afb5482ea42
tense-embedder <joseph.hoekstra@outlook.com>
2025-05-12 09:57:25 +0200

    Exercise 4 - ported from MATLAB to Python

--- machine-learning-ex4/ex4/nnCostFunction.py
+++ machine-learning-ex4/ex4/nnCostFunction.py
@@ -1,105 +1,105 @@
-function [J grad] = nnCostFunction(nn_params, ...
-                                   input_layer_size, ...
-                                   hidden_layer_size, ...
-                                   num_labels, ...
-                                   X, y, lambda)
-%NNCOSTFUNCTION Implements the neural network cost function for a two layer
-%neural network which performs classification
-%   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
-%   X, y, lambda) computes the cost and gradient of the neural network. The
-%   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
-%   The returned parameter grad should be a "unrolled" vector of the
-%   partial derivatives of the neural network.
-%
+#!/usr/bin/env python3
 
-% Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
-% for our 2 layer neural network
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+import numpy as np
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
+from sigmoid import sigmoid
+from sigmoidGradient import sigmoidGradient
 
-% Setup some useful variables
-m = size(X, 1);
+
+def nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambda_value):
+    #NNCOSTFUNCTION Implements the neural network cost function for a two layer
+    #neural network which performs classification
+    #   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
+    #   X, y, lambda) computes the cost and gradient of the neural network. The
+    #   parameters for the neural network are "unrolled" into the vector
+    #   nn_params and need to be converted back into the weight matrices. 
+    # 
+    #   The returned parameter grad should be a "unrolled" vector of the
+    #   partial derivatives of the neural network.
+    #
+
+    # Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
+    # for our 2 layer neural network
+    Theta1 = nn_params[0:hidden_layer_size*(input_layer_size+1)].reshape(
+        hidden_layer_size, input_layer_size+1)
+    Theta2 = nn_params[hidden_layer_size*(input_layer_size+1):].reshape(
+        num_labels, hidden_layer_size+1)
+
+    # Setup some useful variables
+    m, n = X.shape
          
-% You need to return the following variables correctly 
-J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
+    # You need to return the following variables correctly 
+    J = 0
+    Theta1_grad = np.zeros(Theta1.shape)
+    Theta2_grad = np.zeros(Theta2.shape)
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should complete the code by working through the
-%               following parts.
-%
-% Part 1: Feedforward the neural network and return the cost in the
-%         variable J. After implementing Part 1, you can verify that your
-%         cost function computation is correct by verifying the cost
-%         computed in ex4.m
-%
-% Part 2: Implement the backpropagation algorithm to compute the gradients
-%         Theta1_grad and Theta2_grad. You should return the partial derivatives of
-%         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
-%         Theta2_grad, respectively. After implementing Part 2, you can check
-%         that your implementation is correct by running checkNNGradients
-%
-%         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
-%               binary vector of 1's and 0's to be used with the neural network
-%               cost function.
-%
-%         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
-%               first time.
-%
-% Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-%
+    # ====================== YOUR CODE HERE ======================
+    # Instructions: You should complete the code by working through the
+    #               following parts.
+    #
+    # Part 1: Feedforward the neural network and return the cost in the
+    #         variable J. After implementing Part 1, you can verify that your
+    #         cost function computation is correct by verifying the cost
+    #         computed in ex4.m
+    #
+    # Part 2: Implement the backpropagation algorithm to compute the gradients
+    #         Theta1_grad and Theta2_grad. You should return the partial derivatives of
+    #         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
+    #         Theta2_grad, respectively. After implementing Part 2, you can check
+    #         that your implementation is correct by running checkNNGradients
+    #
+    #         Note: The vector y passed into the function is a vector of labels
+    #               containing values from 1..K. You need to map this vector into a 
+    #               binary vector of 1's and 0's to be used with the neural network
+    #               cost function.
+    #
+    #         Hint: We recommend implementing backpropagation using a for-loop
+    #               over the training examples if you are implementing it for the 
+    #               first time.
+    #
+    # Part 3: Implement regularization with the cost function and gradients.
+    #
+    #         Hint: You can implement this around the code for
+    #               backpropagation. That is, you can compute the gradients for
+    #               the regularization separately and then add them to Theta1_grad
+    #               and Theta2_grad from Part 2.
+    #
 
-% Feed forward
-a1 = [ones(m, 1) X];
-z2 = a1 * Theta1';
-a2 = sigmoid(z2);
-a2 = [ones(m, 1) a2];
-z3 = a2 * Theta2';
-h = sigmoid(z3);
+    # Feed forward
+    a1 = np.column_stack([np.ones(m), X])
+    z2 = np.matmul(a1, Theta1.T)
+    a2 = sigmoid(z2)
+    a2 = np.column_stack([np.ones(m), a2])
+    z3 = np.matmul(a2, Theta2.T)
+    h = sigmoid(z3)
 
-% Main term of the cost function
-for k = 1:num_labels
-    yk = y == k;
-    hk = h(:, k);
-    Jk = sum(-yk .* log(hk) - (1 - yk) .* log(1 - hk)) / m;
-    J = J + Jk;
-end
+    # Main term of the cost function
+    for k in range(1, num_labels + 1):
+        yk = (y == k).astype(int)
+        hk = h[:, k - 1]
+        Jk = np.sum(-yk * np.log(hk) - (1 - yk) * np.log(1 - hk)) / m
+        J = J + Jk
 
-% Regularization term of the cost function
-J = J + lambda * (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2))) / (2 * m);
+    # Regularization term of the cost function
+    J = J + lambda_value * (np.sum(np.sum(Theta1[:, 1:] ** 2)) + np.sum(np.sum(Theta2[:, 1:] ** 2))) / (2 * m)
 
-% Backpropagation
-for t = 1:m
-    % For each training sample
-    d3 = zeros(1, num_labels);
-    for k = 1:num_labels
-        yk = y(t) == k;
-        d3(k) = h(t, k) - yk;
-    end
-    d2 = Theta2' * d3' .* sigmoidGradient([1, z2(t, :)])';
-    d2 = d2(2:end);
-    Theta1_grad = Theta1_grad + d2 * a1(t, :);
-    Theta2_grad = Theta2_grad + d3' * a2(t, :);
-end
-% Main term of the gradient
-Theta1_grad = Theta1_grad / m;
-Theta2_grad = Theta2_grad / m;
-% Regularization term of the gradient
-Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda * Theta1(:, 2:end) / m;
-Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda * Theta2(:, 2:end) / m;
+    # Backpropagation
+    for t in range(1, m + 1):
+        # For each training sample
+        d3 = np.zeros((1, num_labels))
+        for k in range(1, num_labels + 1):
+            yk = (y[t - 1] == k).astype(int)
+            d3[0, k - 1] = h[t - 1, k - 1] - yk
+        d2 = np.multiply(np.dot(Theta2.T, d3.T), sigmoidGradient(np.r_[1, z2[t - 1, :]])[None].T)
+        d2 = d2[1:]
+        Theta1_grad = Theta1_grad + np.dot(d2, a1[t - 1, :][None])
+        Theta2_grad = Theta2_grad + np.dot(d3.T, a2[t - 1, :][None])
+    # Main term of the gradient
+    Theta1_grad = Theta1_grad / m
+    Theta2_grad = Theta2_grad / m
+    # Regularization term of the gradient
+    Theta1_grad[:, 1:] = Theta1_grad[:, 1:] + lambda_value * Theta1[:, 1:] / m
+    Theta2_grad[:, 1:] = Theta2_grad[:, 1:] + lambda_value * Theta2[:, 1:] / m
 
 
 
@@ -115,12 +115,13 @@ Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda * Theta2(:, 2:end) / m;
 
 
 
-% -------------------------------------------------------------
+    # -------------------------------------------------------------
 
-% =========================================================================
+    # =========================================================================
 
-% Unroll gradients
-grad = [Theta1_grad(:) ; Theta2_grad(:)];
+    # Unroll gradients
+    grad = np.concatenate([Theta1_grad.ravel(), Theta2_grad.ravel()])
 
 
-end
+    return (J, grad)
+    #end
--- machine-learning-ex4/ex4/predict.py
+++ machine-learning-ex4/ex4/predict.py
@@ -1,20 +1,28 @@
-function p = predict(Theta1, Theta2, X)
-%PREDICT Predict the label of an input given a trained neural network
-%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-%   trained weights of a neural network (Theta1, Theta2)
+#!/usr/bin/env python3
 
-% Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+import numpy as np
 
-% You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
-
-h1 = sigmoid([ones(m, 1) X] * Theta1');
-h2 = sigmoid([ones(m, 1) h1] * Theta2');
-[dummy, p] = max(h2, [], 2);
-
-% =========================================================================
+from sigmoid import sigmoid
 
 
-end
+def predict(Theta1, Theta2, X):
+    #PREDICT Predict the label of an input given a trained neural network
+    #   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+    #   trained weights of a neural network (Theta1, Theta2)
+
+    # Useful values
+    m = X.shape[0]
+    num_labels = Theta2.shape[0]
+
+    # You need to return the following variables correctly 
+    p = np.zeros((X.shape[0], 1))
+
+    h1 = sigmoid(np.dot(np.column_stack([np.ones((m, 1)), X]), Theta1.T))
+    h2 = sigmoid(np.dot(np.column_stack([np.ones((m, 1)), h1]), Theta2.T))
+    p = np.argmax(h2, axis = 1) + 1
+
+    # =========================================================================
+
+
+    return p
+    #end
