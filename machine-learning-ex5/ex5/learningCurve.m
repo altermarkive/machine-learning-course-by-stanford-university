884d912711d861f7d7948e817fa4def54a376485
tense-embedder <joseph.hoekstra@outlook.com>
2025-05-12 09:57:23 +0200

    Exercise 5 - solved

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,6 +53,13 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+    X_i = X(1:i, :);
+    y_i = y(1:i);
+    theta_i = trainLinearReg(X_i, y_i, lambda);
+    error_train(i) = linearRegCostFunction(X_i, y_i, theta_i, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta_i, 0);
+end
 
 
 
